91c96579237bee336696a34f4ca3ea0b6b542f0f
Akmargam <132103081+Akmargam@users.noreply.github.com>
2024-08-20 11:02:33 -0400

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -4,7 +4,7 @@ sudo apt update -y
 sudo apt install openjdk-17-jdk -y
 sudo apt update -y
 sudo apt install maven -y
-wget -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
